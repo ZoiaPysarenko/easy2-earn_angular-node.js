8295e8db9dade4ff078e98395cbce9dea1904ec4
Zoya <zjamaozja@gmail.com>
2020-07-06 18:51:07 +0300

    DB connection added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+// Database Postgre
+const { Pool } = require('pg');
+
+//connecting to DB
+//the constructor of Pool takes a configuration json object {conncetionString:"",ssl: { rejectUnauthorized: false }}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+//CREATE Table?
+router.get('/createtables', function(req, res, next){})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
